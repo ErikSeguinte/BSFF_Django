56df01c39ee731bfb1a62136f1fa0d5438136c85
Erik <spamfactorx01@gmail.com>
2021-05-16 17:14:27 -0700

    initial commit

---
+++ bsff/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bsff.users.models import User
+from bsff.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
