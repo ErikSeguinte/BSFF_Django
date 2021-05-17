7c880a98ff49af66005e46754117b03cfb08b0be
Erik <spamfactorx01@gmail.com>
2021-05-16 17:41:07 -0700

    adds actor model

---
+++ bsff/actors/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ActorsConfig(AppConfig):
+    name = 'bsff.actors'
---
+++ bsff/actors/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from autoslug import AutoSlugField
+from model_utils.models import TimeStampedModel
+
+# Create your models here.
+
+class Actor(TimeStampedModel):
+    full_name = models.CharField("Full Name", max_length=255)
+    slug = AutoSlugField("Actor URL",
+        unique=True, always_update=True, populate_from="full_name")
+    short_name = models.CharField("First name, short name, or use-name", max_length=255)
+
+    bio = models.TextField("Bio", )
